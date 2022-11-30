c7c444ccfbcef3b218043a9744888e9e0daff5f9
artmenam <artmenam@mail.ru>
2022-11-30 21:05:44 +0300

    init

---
+++ bboard/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True,blank=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    balance = models.FloatField(null=True,blank=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
+
+
+class StockRn(models.Model):
+    nameCompany = models.CharField(max_length=50)
+    currentPrice = models.FloatField(null=True, blank=True)
