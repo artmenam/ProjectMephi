55fc905e83f60524223c949b6eaed62df9d277ee
artmenam <artmenam@mail.ru>
2022-12-17 23:18:07 +0300

    init

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, predict1
 
 urlpatterns = [
     path('index/', index),
+    path('predict/',predict1)
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 
 from .models import Bb
+from .predict import predict
 
 def index(request):
     s = 'Список объявлений:\r\n\r\n\r\n'
@@ -8,4 +9,9 @@ def index(request):
         s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
     return HttpResponse(s, content_type='text/plain; charset=utf-8')
 
-
+def predict1(request):
+    from1 = request.GET['from']
+    to = request.GET['to']
+    OUT_DIR = '../../data'
+    s, y = predict(from1,to)
+    return HttpResponse(s,y, content_type='text/plain; charset=utf-8')
